f90bbab35adba467fe3f3480282af2c9924beeb1
Alex-Jr <jr.alexmendes@gmail.com>
2020-08-14 16:40:02 -0300

    Mudando retorno

--- list.js
+++ list.js
@@ -23,7 +23,7 @@ export const main = async (event, context, callback) => {
       const response = {
         statusCode: 200,
         headers: headers,
-        body: JSON.stringify(data),
+        body: JSON.stringify(data.Items),
       };
       callback(null, response);
       return;
